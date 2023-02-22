bc476f124922a02902ff9058595d69499e0a7f02
DarjaNevrova <122455394+DarjaNevrova@users.noreply.github.com>
2023-02-22 15:36:20 +0200

    Add files via upload

---
+++ Homework_17_02/script/script.js
@@ -0,0 +1,29 @@
+const emerald = document.querySelector('.btn_emerald');
+
+emerald.addEventListener('click', function(){
+    alert('#2ecc71')
+})
+
+const wisteria = document.querySelector('.btn_wisteria');
+
+wisteria.addEventListener('click', function(){
+    alert('#8e44ad')
+})
+
+const yellow = document.querySelector('.btn_yellow');
+
+yellow.addEventListener('click', function(){
+    alert('#f1c40f')
+})
+
+const blue = document.querySelector('.btn_blue');
+
+blue.addEventListener('click', function(){
+    alert('#2980b9')
+})
+
+const pink = document.querySelector('.btn_pink');
+
+pink.addEventListener('click', function(){
+    alert('#ff7979')
+})
